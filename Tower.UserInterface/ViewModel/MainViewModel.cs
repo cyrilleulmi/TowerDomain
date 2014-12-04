6b1e326e86e6f42a7f6eb9839705d04046aeb6b9
juventino07 <crips_4@hotmail.com>
2014-12-04 13:56:35 +0100

    GUI

---
+++ Tower.UserInterface/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.ComponentModel;
+
+namespace Tower.UserInterface.ViewModel
+{
+    class MainViewModel :Window, INotifyPropertyChanged
+    {
+        private int range;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+
+        }
+
+        public int DisplayRange
+        {
+            get 
+            {
+                return range;
+            }
+            set
+            {
+                if (value != range)
+                {
+                    range = value;
+                    NotifyPropertyChanged("DisplayRange");
+                }
+            }
+        }
+        
+
+    private void NotifyPropertyChanged(String info)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
+}
