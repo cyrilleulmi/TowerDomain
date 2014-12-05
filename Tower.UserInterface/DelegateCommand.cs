7eb54fd916d79963b47f564886cfd64a839c7f0f
juventino07 <crips_4@hotmail.com>
2014-12-05 17:39:09 +0100

    TopCommit

---
+++ Tower.UserInterface/DelegateCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SystemUpdate
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
